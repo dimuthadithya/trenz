ef33f5613e307d11dfd6125f89ba93f6256c5292
dimuthx353 <dimuthx353@gmail.com>
2025-03-14 11:31:59 +0530

    fix WishlistCard.php

--- app/View/Components/WishlistCard.php
+++ app/View/Components/WishlistCard.php
@@ -6,11 +6,8 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class wishlist-card extends Component
+class WishlistCard extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public function __construct()
     {
         //
