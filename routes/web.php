1e0a634a7ac275d92dabcd4865ab6f8b9bc132a4
dimuthx353 <dimuthx353@gmail.com>
2025-03-15 21:00:08 +0530

    add some comments for routes

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,21 @@ use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
 
+// route for home page 
 Route::get('/', function () {
     return view('index');
 })->name('home');
 
+// Routes for pages 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
 Route::post('/register', [AuthenticatedSessionController::class, 'store'])->name('register');
 Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 Route::get('/men', [ProductController::class, 'indexMen'])->name('men');
 Route::get('/women', [ProductController::class, 'indexWomen'])->name('women');
 Route::get('/kids', [ProductController::class, 'indexKid'])->name('kid');
-
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
 
+// Routes for Reg User 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -41,12 +43,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/users', [UsersController::class, 'index'])->name('users.index');
 });
 
+// Routes for Admin 
 Route::middleware(['auth', AdminMiddleware::class])->name('admin.')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
 });
-
-
-
 
 
 
