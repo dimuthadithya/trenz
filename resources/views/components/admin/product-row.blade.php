432e1700b8f6a21d7fde7857da0a124d1986d6a9
dimuthx353 <dimuthx353@gmail.com>
2025-03-18 16:02:55 +0530

    add modal for admin product page

--- resources/views/components/admin/product-row.blade.php
+++ resources/views/components/admin/product-row.blade.php
@@ -12,7 +12,30 @@
                         <div class="img" style="background-image: url(images/product-1.png);"></div>
                     </td>
                     <td>
-                        <x-text-input type=" text" value="{{ $product['description'] }}" />
+                        <!-- <x-text-input type=" text" value="{{ $product['description'] }}" /> -->
+                        <!-- Button trigger modal -->
+                        <input type="text" class="form-control" data-bs-toggle="modal" data-bs-target="#exampleModal" value="{{ $product['description'] }}">
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">{{ $product['name'] }}</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <textarea id="mytextarea">
+                                        {{ $product['description'] }}
+                                        </textarea>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </td>
                     <td>
                         <x-text-input type="text" value="{{ $product['price'] }}" />
@@ -21,14 +44,15 @@
                         <x-text-input type="text" value="{{ $product['stock'] }}" />
                     </td>
                     <td>
-                        <x-text-input type="text" value="{{ $product['category_id'] }}" />
+                        <button class="btn btn-success btn-sm"><i class=" fas fa-check-circle"></i></button>
                     </td>
                     <td>
+                        <x-text-input type="text" value="{{ $product['category_id'] }}" />
+                    </td>
+                    <td class="">
                         <button class="btn btn-danger btn-sm">
-                            <i class="fa fa-shopping-cart"></i>
-                        </button>
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true"><i class="fa fa-close"></i></span>
+                            <!-- <span>Remove</span> -->
+                            <i class="icon-trash"></i>
                         </button>
                     </td>
                 </tr>
--- resources/views/pages/admin/products/index.blade.php
+++ resources/views/pages/admin/products/index.blade.php
@@ -4,12 +4,6 @@
       @endphp
 
       <x-admin-app-layout>
-          @push('styles')
-          <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-          <link rel="stylesheet" href="{{ asset('assets/css/wishlist/css/style.css') }}">
-          @endpush
-
           <div class="container mt-5">
               <section class=" ftco-section">
                   <div class="row">
@@ -24,6 +18,7 @@
                                           <th>Description</th>
                                           <th>Price</th>
                                           <th>Stock</th>
+                                          <th>&nbsp;</th>
                                           <th>Category</th>
                                           <th>Actions</th>
                                       </tr>
@@ -40,10 +35,4 @@
               </section>
           </div>
 
-          @push('scripts')
-          <script src=" {{ asset('assets/js/wishlist/js/jquery.min.js') }}"></script>
-          <script src="{{ asset('assets/js/wishlist/js/popper.js') }}"></script>
-          <script src="{{ asset('assets/js/wishlist/js/bootstrap.min.js') }}"></script>
-          <script src="{{ asset('assets/js/wishlist/js/main.js') }}"></script>
-          @endpush
       </x-admin-app-layout>
