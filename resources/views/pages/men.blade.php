261fac9f35ff1dbc66c960360fa0a93add6625a5
dimuthx353 <dimuthx353@gmail.com>
2025-05-02 06:51:45 +0530

    change currency to lkr and update men page

--- resources/views/pages/men.blade.php
+++ resources/views/pages/men.blade.php
@@ -1,10 +1,33 @@
-<x-app-layout>
+<!-- <x-app-layout>
     <section class="flex-wrap product spad d-flex">
         @include('layouts.filters')
         <div class="container col-lg-9 col-md-6 col-12">
             <div class="row">
                 @foreach($products as $product)
                 <x-product-card :product="$product" />
+                @endforeach
+            </div>
+        </div>
+    </section>
+</x-app-layout> -->
+
+@php
+use App\Models\Category;
+@endphp
+
+<x-app-layout>
+    <section class="flex-wrap product spad d-flex">
+        @include('layouts.filters',['category' => 'men'])
+        <div class="container col-lg-9 col-md-6 col-12">
+            <button class="mb-4 btn btn-primary btn-sm" id="showing_category_btn">
+                <span id="showing_category_text" class="me-3">All</span><i class="fa-solid fa-xmark"></i>
+            </button>
+            <div class="row" id="product-list">
+                @foreach($products as $product)
+                @php
+                $categoryName = Category::where('id', $product->category_id)->value('category_name');
+                @endphp
+                <x-product-card :product="$product" class="{{ $categoryName }}" />
                 @endforeach
             </div>
         </div>
