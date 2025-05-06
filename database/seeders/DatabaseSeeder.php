1d644456a008e287d8d8d658ff8bbc28aa67690a
dimuthx353 <dimuthx353@gmail.com>
2025-05-06 15:49:10 +0530

    add rating column to the db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(AdminSeeder::class);
-        // $this->call(CategorySeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(CategorySeeder::class);
         $this->call(productsMenSeedr::class);
-        // $this->call(productsWomenSeedr::class);
+        $this->call(productsWomenSeedr::class);
     }
 }
