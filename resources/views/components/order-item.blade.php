7478bf24604881694f87249ea685d0ecbb9c19c3
dimuthx353 <dimuthx353@gmail.com>
2025-05-07 11:47:25 +0530

    create order item single component

---
+++ resources/views/components/order-item.blade.php
@@ -0,0 +1,21 @@
+        <div class="order-item">
+            <div class="order-header">
+                <div class="order-vendor">
+                    <span class="badge bg-secondary">M</span> Daraz Top-ups
+                </div>
+                <div class="order-status">
+                    <span class="delivered-label">Delivered</span>
+                </div>
+            </div>
+            <div class="order-content">
+                <div class="order-img">
+                    <img src="/api/placeholder/60/60" alt="Dialog" class="img-fluid">
+                </div>
+                <div class="order-details">
+                    <div class="order-title">Dialog - Rs. 300 - Dialog Prepaid Rs 300</div>
+                    <div class="order-info">Phone Number: 0740069520</div>
+                </div>
+                <div class="order-price">Rs. 300</div>
+                <div class="order-qty">Qty: 1</div>
+            </div>
+        </div>
