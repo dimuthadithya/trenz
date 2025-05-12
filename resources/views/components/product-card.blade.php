b24935f5d461b55d99a766cfc5bce13557c86c15
dimuthx353 <dimuthx353@gmail.com>
2025-05-12 09:41:25 +0530

    fix center alignmnets for product card in hover state

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -6,13 +6,13 @@ $product_id = $product->id;
 $categoryName = Category::where('id', $product->category_id)->value('category_name');
 @endphp
 
-<div {{ $attributes->merge(['class' => 'col-lg-3 col-md-4 col-sm-6 product-card-item']) }} onclick="window.location='{{ route('product.show', $product->id) }}'">
+<div {{ $attributes->merge(['class' => 'col-lg-3 col-md-4 col-sm-6 product-card-item']) }}>
     <div class="cursor-pointer product__item">
-        <div class="product__item__pic set-bg" data-setbg="../{{ $product->image ?? asset('assets/img/product/product-1.jpg') }}">
+        <div class="product__item__pic set-bg" data-setbg="../{{ $product->image  }}">
             <div class="label new">{{$categoryName }}</div>
             <ul class="product__hover">
                 <li>
-                    <a href="{{ $product->image ?? asset('assets/img/product/product-1.jpg') }}" class="image-popup"><span class="arrow_expand"></span></a>
+                    <a href="{{ asset($product->image) }}" class="image-popup"><span class="arrow_expand"></span></a>
                 </li>
                 <li>
                     @auth
@@ -34,7 +34,7 @@ $categoryName = Category::where('id', $product->category_id)->value('category_na
         </div>
         <div class="pb-3 border product__item__text d-flex flex-column ctz-height">
             <div class="flex-grow-1">
-                <h5><a href="#">{{ $product->name ?? 'Buttons tweed blazer' }}</a></h5>
+                <h5><a href="{{ route('product.show', $product_id) }}">{{ $product->name ?? 'Buttons tweed blazer' }}</a></h5>
             </div>
             <div class="mb-2 rating">
                 @php
