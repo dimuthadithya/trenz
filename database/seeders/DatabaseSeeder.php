ed0b7840bfbe81186f545f25610e17161b4a2c4f
dimuthx353 <dimuthx353@gmail.com>
2025-06-04 12:52:21 +0530

    Add UserSeeder to seed initial user data in the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(AdminSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(productsMenSeedr::class);
         $this->call(productsWomenSeedr::class);
