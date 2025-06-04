ba82a81cc7d658c8926d0856c37678e8ba3f6d39
dimuthx353 <dimuthx353@gmail.com>
2025-06-04 16:21:45 +0530

    Fix indentation in breadcrumb section of checkout page

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -5,16 +5,16 @@ use App\Models\Cart;
 <x-app-layout>
     <!-- Breadcrumb Begin -->
     <div class="breadcrumb-option">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="breadcrumb__links">
-                        <a href="{{ route('home') }}"><i class="fa fa-home"></i> Home</a>
-                        <span>Shopping cart</span>
-                    </div>
+        <div class="container"></div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb__links">
+                    <a href="{{ route('home') }}"><i class="fa fa-home"></i> Home</a>
+                    <span>Shopping cart</span>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <!-- Breadcrumb End -->
 
